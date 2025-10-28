0efaec1777e4bf3e8d26cd7eed3c444a21e18f5a
Ainun Fashia Aksan <158124428+ainunfashiaa@users.noreply.github.com>
2025-10-28 23:05:13 +0700

    Add index.php for PHP CI/CD pipeline demo

---
+++ src/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>🚀 Hello, I'm Ainun! I hope this code PHP CI/CD Pipeline is running successfully!</h1>";
+echo "<p>Build and deployed automatically via GitHub Actions.</p>";
+echo "<p>Image source: dockerhub_aynun/php-demo:latest</p>";
+?>
